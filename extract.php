fdcd830bd664b66c9d4345a16f8020253a785e5b
mjreinoso <mreinoso@uninorte.edu.co>
2023-09-13 21:38:53 -0500

    Style change, PHP security update, Leaflet map edit

--- extract.php
+++ extract.php
@@ -1,7 +1,11 @@
 <?php
 error_reporting(E_ALL);
-// Conexión a la base de datos (debes configurar las credenciales)
-$conexion = new mysqli("fmwbase.c49tf0faxvcu.us-east-2.rds.amazonaws.com", "admin", "m12345678", "fmwdb");
+
+// Leer las credenciales desde el archivo de configuración
+$config = parse_ini_file('config.ini');
+
+// Conexión a la base de datos
+$conexion = new mysqli($config['host'], $config['username'], $config['password'], $config['dbname']);
 
 // Verificar la conexión
 if ($conexion->connect_error) {
@@ -9,20 +13,17 @@ if ($conexion->connect_error) {
 }
 
 // Consulta SQL para obtener el último registro de la tabla
-//$sql = "SELECT latitude, longitude, DATE_FORMAT(time_stamp, '%Y-%m-%d %H:%i:%s') AS formatted_time_stamp FROM location ORDER BY time_stamp DESC L>$sql = "SELECT latitude, longitude, time_stamp FROM location ORDER BY time_stamp DESC LIMIT 30";
 $sql = "SELECT latitude, longitude, time_stamp FROM ubication ORDER BY time_stamp DESC LIMIT 30";
 
 $resultado = $conexion->query($sql);
 
 if ($resultado->num_rows > 0) {
     $fila = $resultado->fetch_assoc();
-  //  $timestamp = strtotime($fila["time_stamp"])
-   // $fechaHora = date("Y/m/d H:i:s", $timestamp)
-     // Convertir los datos en un arreglo asociativo
+    
+    // Convertir los datos en un arreglo asociativo
     $datos = array(
         "latitude" => $fila["latitude"],
         "longitude" => $fila["longitude"],
-      //"time_stamp" => $time_stamp // Aquí se usa la fecha y hora decodificada
         "time_stamp" => $fila["time_stamp"]
     );
     echo json_encode($datos);
@@ -33,4 +34,5 @@ if ($resultado->num_rows > 0) {
 // Cerrar la conexión a la base de datos
 $conexion->close();
 ?>
+
 
