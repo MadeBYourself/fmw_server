0288f0a6113613f5d1bf0e07309e6f05da360084
mjreinoso <mreinoso@uninorte.edu.co>
2023-09-21 15:54:10 -0500

    Update php

--- extract2.php
+++ extract2.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
 
 if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['start-datetime']) && isset($_GET['end-datetime'])) {
     $startTimestamp = strtotime($_GET['start-datetime']) * 1000;
-    $endTimestamp = (strtotime($_GET['end-datetime']) + 18000) * 1000;
+    $endTimestamp = (strtotime($_GET['end-datetime']) + 86400) * 1000;
 
     // Consulta SQL para buscar en el rango de tiempo
     $sql = "SELECT latitude, longitude, time_stamp FROM ubication WHERE time_stamp >= $startTimestamp AND time_stamp <= $endTimestamp";
@@ -43,8 +43,6 @@ if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['start-datetime']) && is
 
     // Convertir el array a JSON y mostrarlo
     echo json_encode($response);
-    echo json_encode($startTimestamp);
-    echo json_encode($endTimestamp);
 }
 
 $conn->close();
