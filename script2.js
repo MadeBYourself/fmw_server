ce1cf6000037cfaa4af7c88bb186f4ccc790a201
MigueJVargas <vargasviloriam5@gmail.com>
2023-10-03 14:31:05 -0500

    added the radius slider and a popup for a selected area where is no data

--- script2.js
+++ script2.js
@@ -27,14 +27,14 @@ if (xhr.status === 200) {
 
 // Función para inicializar el mapa
 function initMap() {
-    
+
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-  
+
     const polyline = L.polyline([], { color: "red" }).addTo(map);
-  
+
     // Escucha los eventos de entrada en los campos de fecha y hora
     document
       .getElementById("start-datetime")
@@ -42,14 +42,14 @@ function initMap() {
         const startDateTime = event.target.value;
         document.getElementById("end-datetime").min = startDateTime;
       });
-  
+
     document
       .getElementById("end-datetime")
       .addEventListener("input", function (event) {
         const endDateTime = event.target.value;
         document.getElementById("start-datetime").max = endDateTime;
       });
-  
+
     // Escucha el evento de envío del formulario
     document
       .getElementById("search-form")
@@ -58,24 +58,26 @@ function initMap() {
         handleFormSubmit(map, polyline);
       });
   }
-  
+
   // Función para manejar el envío del formulario
   function handleFormSubmit(map, polyline) {
     const startDateTime = document.getElementById("start-datetime").value;
     const endDateTime = document.getElementById("end-datetime").value;
-  
+
     // Hacer una solicitud al servidor para obtener las coordenadas
     const url = `extract2.php?start-datetime=${startDateTime}&end-datetime=${endDateTime}`;
     selectedMarker.setOpacity(0)
-    slider.value = 0;  // Restablecer el valor del slider
-    timestampDisplay.innerText = '';  // Borrar el texto de timestamp
+    slider.value = 0;
+    slider2.value = 0;  // Restablecer el valor del slider
+    timestampDisplay.innerText = '';
+    radiusDisplay.innerText = "";  // Borrar el texto de timestamp
     // Remover los círculos existentes
     bigCircle && map.removeLayer(bigCircle);
     smallCircles.forEach(function(circle) {
         map.removeLayer(circle);
     });
     smallCircles = [];
-  
+
     fetch(url)
       .then((response) => response.json())
       .then((response) => {
@@ -85,7 +87,7 @@ function initMap() {
             parseFloat(coord.longitude),
           ]);
           polyline.setLatLngs(latlngs);
-  
+
           // Centrar el mapa en las coordenadas de la polilínea
           const bounds = polyline.getBounds();
           map.fitBounds(bounds);
@@ -113,21 +115,31 @@ function initMap() {
   slider.min = 0;
   slider.value = 0;
 
+  var slider2 = document.createElement("input");
+  slider2.type = 'range';
+  slider2.min = 0;
+  slider2.max = 500;
+  slider2.value = 0;
+
+
   // Agregar una clase para estilizar el slider con CSS
   slider.classList.add('slider');
+  slider.classList.add("slider2");
 
   // Agregar el slider al contenedor
   contenedor.appendChild(slider);
+  contenedor.appendChild(slider2);
 
   // Crear un elemento para mostrar el timestamp
   var timestampDisplay = document.createElement('div');
+  var radiusDisplay = document.createElement ("div");
 
   // Agregar una clase para estilizar el elemento con CSS
   timestampDisplay.classList.add('time_display');
-
+  radiusDisplay.classList.add("time_display");
   // Agregar el elemento al contenedor
   contenedor.appendChild(timestampDisplay);
-
+  contenedor.appendChild(radiusDisplay);
 
   // Obtener una referencia al botón
   var toggleButton = document.getElementById('toggleButton');
@@ -147,8 +159,12 @@ function initMap() {
     const startDateTime = document.getElementById("start-datetime").value;
     const endDateTime = document.getElementById("end-datetime").value;
     selectedMarker.setOpacity(0)
-    slider.value = 0;  // Restablecer el valor del slider
-    timestampDisplay.innerText = '';  // Borrar el texto de timestamp
+    slider.value = 0;
+      // Restablecer el valor del slider
+    timestampDisplay.innerText = '';
+      slider2.addEventListener('input', function() {
+        radiusDisplay.innerText = "The radius in meters of your next area to pick is: " +slider2.value;
+           });
     // Remover los círculos existentes
     bigCircle && map.removeLayer(bigCircle);
     smallCircles.forEach(function(circle) {
@@ -157,7 +173,7 @@ function initMap() {
     smallCircles = [];
 
     bigCircle = L.circle(e.latlng, {
-        radius: 30,
+        radius: slider2.value,
         color: 'blue',
         fillColor: 'blue',
         fillOpacity: 0.5
@@ -178,51 +194,53 @@ function initMap() {
         data: requestData,
         dataType: 'json',
         success: function(data) {
-            console.log('Time_stamps dentro del círculo y lapso de tiempo:', data);
+         if(Array.isArray(data)&& data.length > 0){
+          console.log('Time_stamps dentro del círculo y lapso de tiempo:', data);
 
-            // Mostrar los puntos en el mapa
-            for (var i = 0; i < data.length; i++) {
-                var point = data[i];
-                var latLng = L.latLng(point.latitude, point.longitude);
-                var circle = L.circle(latLng, {
-                  radius: 1, // Radio del círculo (ajusta según tu preferencia)
-                  color: 'red',
-                  fillColor: 'red',
-                  fillOpacity: 0.5
-                }).addTo(map);
-              circle.timestamp = point.time_stamp;
-              smallCircles.push(circle);
+          // Mostrar los puntos en el mapa
+          for (var i = 0; i < data.length; i++) {
+              var point = data[i];
+              var latLng = L.latLng(point.latitude, point.longitude);
+              var circle = L.circle(latLng, {
+                radius: 1, // Radio del círculo (ajusta según tu preferencia)
+                color: 'red',
+                fillColor: 'red',
+                fillOpacity: 0.5
+              }).addTo(map);
+            circle.timestamp = point.time_stamp;
+            smallCircles.push(circle);
+          }
+
+        // Actualizar el valor máximo del slider según la cantidad de timestamps
+        slider.max = data.length - 1;
+
+        // Asociar evento al slider para actualizar el timestamp y resaltar el círculo
+        slider.addEventListener('input', function() {
+            var selectedTimestamp = data[this.value].time_stamp;
+            timestampDisplay.innerHTML = 'Date: ' + selectedTimestamp;
+
+            // Remover el resaltado de los círculos
+            smallCircles.forEach(function(circle) {
+                circle.setStyle({ color: 'red' });
+            });
+
+            // Resaltar el círculo correspondiente al timestamp seleccionado
+            var selectedCircle = smallCircles.find(function(circle) {
+                return circle.timestamp === selectedTimestamp;
+            });
+            if (selectedCircle) {
+                selectedCircle.setStyle({ color: 'black' });
+                selectedMarker.setLatLng(selectedCircle.getLatLng()).setOpacity(1);
             }
-
-          // Actualizar el valor máximo del slider según la cantidad de timestamps
-          slider.max = data.length - 1;
-
-          // Asociar evento al slider para actualizar el timestamp y resaltar el círculo
-          slider.addEventListener('input', function() {
-              var selectedTimestamp = data[this.value].time_stamp;
-              timestampDisplay.innerHTML = 'Date: ' + selectedTimestamp;
-
-              // Remover el resaltado de los círculos
-              smallCircles.forEach(function(circle) {
-                  circle.setStyle({ color: 'red' });
-              });
-
-              // Resaltar el círculo correspondiente al timestamp seleccionado
-              var selectedCircle = smallCircles.find(function(circle) {
-                  return circle.timestamp === selectedTimestamp;
-              });
-              if (selectedCircle) {
-                  selectedCircle.setStyle({ color: 'black' });
-                  selectedMarker.setLatLng(selectedCircle.getLatLng()).setOpacity(1);
-              }
-          });
-      },
-      error: function(xhr, status, error) {
-          console.error('Error al obtener los datos:', error);
-      }
-  });
+        });
+      }else{
+        alert("there is no data on this area");
+      };
+    },
+    error: function(xhr, status, error) {
+        console.error('Error al obtener los datos:', error);
+    }
+});
 }
 
-
-  initMap();
-  
+initMap();
